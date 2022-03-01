24360056cf30c581676b5b72ef66f6051e98fa24
songdaegeun <eorms6199@naver.com>
2022-03-01 16:16:34 +0900

    feat: Add car_set.dat, Add save_load.c
    "car_state" save to car_set.dat
    made save and load function about entering and car_set.dat
    
    footer None

--- include/enter_exit_manage.h
+++ include/enter_exit_manage.h
@@ -4,15 +4,15 @@
 #include "array.h"
 
 typedef struct Person{
-    int dong;           //동 ex)NULL이면 임직원아님, 나머지는 임직원
-    int contac_num;     //연락처
+    int dong;           //동 ex)0이면 임직원아님, 나머지는 임직원
+    const char contac_num[15];     //연락처 ex) 01012345678
     // char *memo;
 } Person;
 
 typedef struct Location{
-    char *floor;     //층 ex) B1,B2,B3
-    int row;        //행 ex) 01
-    int col;        //열 ex) 04
+    char floor[5];      //층 ex) B1,B2,B3
+    int row;            //행 ex) 01
+    int col;            //열 ex) 04
 } Location;
 
 typedef struct Time{
@@ -25,17 +25,18 @@ typedef struct Time{
 } Time;
 
 typedef struct Car_state{
-    const char *car_type;       //차종
-    const char *color;          //색깔
-    const char *plate_num;      //차번호
+    const char car_type[10];       //차종
+    const char color[10];          //색깔
+    const char plate_num[15];      //차번호
+    
     Location location;          //주차위치
     Time enter_now;             //입차시각
     Time exit_now;              //출차시각
     Person person;
-} Car_state;
+} Car_state;    //120byte
 
 int enter_exit_time(Car_state *car);
-int car_state_append(LPARRAY lpArray, int car_num);
+int car_state_append(LPARRAY lpInput, LPARRAY lpCarset, int car_num);
 int car_state_remove(LPARRAY lpArray, int car_num);
 
 #endif 
